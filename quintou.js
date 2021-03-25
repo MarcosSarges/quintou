81bd661967cf995ce8e623ee812db7f141c7b6fd
Marcos Paulo <marcos.sarges@pixter.com.br>
2021-03-25 11:44:48 -0300

    fix

--- quintou.js
+++ quintou.js
@@ -4,13 +4,13 @@ const { createReadStream } = require("fs");
 const { Decoder } = require("lame");
 const Speaker = require("speaker");
 
-if (process.versions.node.split(".")[0] === 10) {
-  createReadStream("./quintou.mp3")
-    .pipe(new Decoder())
-    .on("format", () =>
-      console.log("QUINTOU PORRRAAAAAAA!!!!!!!!!!!!!!!!: github/MarcosSarges")
-    )
-    .pipe(new Speaker());
-} else {
-  console.log(`O script só roda na V10 ${JSON.stringify(process.versions)}`);
-}
+// if (process.versions.node.split(".")[0] === 10) {
+createReadStream("./quintou.mp3")
+  .pipe(new Decoder())
+  .on("format", () =>
+    console.log("QUINTOU PORRRAAAAAAA!!!!!!!!!!!!!!!!: github/MarcosSarges")
+  )
+  .pipe(new Speaker());
+// } else {
+//   console.log(`O script só roda na V10 ${JSON.stringify(process.versions)}`);
+// }
