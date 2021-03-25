4e5b4f0507be35ee239e9304fee7067722fc5222
Marcos Paulo <marcos.sarges@pixter.com.br>
2021-03-25 11:29:05 -0300

    fix

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const { createReadStream } = require("fs");
 const { Decoder } = require("lame");
 const Speaker = require("speaker");
 
-createReadStream("./quintou.mp3")
-  .pipe(new Decoder())
-  .on("format", () =>
-    console.log("QUINTOU PORRRAAAAAAA!!!!!!!!!!!!!!!!: github/MarcosSarges")
-  )
-  .pipe(new Speaker());
+if (process.versions.node.split(".")[0] === 10) {
+  createReadStream("./quintou.mp3")
+    .pipe(new Decoder())
+    .on("format", () =>
+      console.log("QUINTOU PORRRAAAAAAA!!!!!!!!!!!!!!!!: github/MarcosSarges")
+    )
+    .pipe(new Speaker());
+} else {
+  console.log("O script só roda na V10");
+}
